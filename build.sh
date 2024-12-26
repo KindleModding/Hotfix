ed53dcdad2ebb25dbd50363083fc2724b210b166
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2024-12-26 09:50:55 +0000

    Make hotfix build native objects
    Should lead to more feature-complete FBInk compared to what is normally shipped

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 ###
 # Check admin
 ##
-if [ $EUID -ne 0 ]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
+#if [ $EUID -ne 0 ]; then
+#   echo "This script must be run as root" 
+#   exit 1
+#fi
 
 export KT_WITH_UNKNOWN_DEVCODES="1"
 
@@ -20,6 +20,43 @@ KINDLETOOL="${KINDLETOOL:-${PWD}/utils/kindletool}"
 ###
 rm -rf ./build ./build_tmp ./tmp_build_cache
 mkdir ./build ./build_tmp ./build_cache
+
+###
+# Build native stuff
+###
+echo "* Building natives"
+mkdir -p ./src/armel/libs/
+mkdir -p ./src/armhf/libs/
+
+echo "* Building sh_integration..."
+cd sh_integration
+sh ./gen_crosscompile.sh
+meson setup --cross-file kindlepw2.txt builddir_armel
+meson setup --cross-file kindlehf.txt builddir_armhf
+cd builddir_armel
+meson compile
+cd ..
+cd builddir_armhf
+meson compile
+cd ../../
+echo "* Copying sh_integration"
+cp ./sh_integration/builddir_armel/extractor/sh_integration_extractor.so ./src/armel/
+cp ./sh_integration/builddir_armel/launcher/sh_integration_launcher ./src/armel/
+cp ./sh_integration/builddir_armhf/extractor/sh_integration_extractor.so ./src/armhf/
+cp ./sh_integration/builddir_armhf/launcher/sh_integration_launcher ./src/armhf/
+
+echo "* Building fbink..."
+cd FBInk
+source ../koxtoolchain/refs/x-compile.sh kindlepw2 env
+KINDLE=1 DRAW=1 BITMAP=1 FONTS=1 IMAGE=1 OPENTYPE=1 INPUT=1 make release
+cp ./Release/fbink ../src/armel/
+cp ./Release/libfbink* ../src/armel/libs/
+make clean
+source ../koxtoolchain/refs/x-compile.sh kindlehf env
+KINDLE=1 DRAW=1 BITMAP=1 FONTS=1 IMAGE=1 OPENTYPE=1 INPUT=1 make release
+cp ./Release/fbink ../src/armhf/
+cp ./Release/libfbink* ../src/armhf/libs/
+cd ..
 
 ###
 # Generate the updater_keys.sqsh file
@@ -55,14 +92,14 @@ echo "* Extracting and mounting official firmware"
 ${KINDLETOOL} extract ./build_cache/update_kindle_pw6.bin ./build_tmp/official_firmware
 gunzip ./build_tmp/official_firmware/*rootfs*.img.gz
 mkdir ./build_tmp/official_firmware_mnt/
-mount -o loop ./build_tmp/official_firmware/*rootfs*.img ./build_tmp/official_firmware_mnt/
+sudo mount -o loop ./build_tmp/official_firmware/*rootfs*.img ./build_tmp/official_firmware_mnt/
 
 echo "* Patching UKS SQSH"
 mkdir ./build_tmp/patched_uks
 mkdir ./build_tmp/mounted_sqsh
-mount -o loop ./build_tmp/official_firmware_mnt/etc/uks.sqsh ./build_tmp/mounted_sqsh
+sudo mount -o loop ./build_tmp/official_firmware_mnt/etc/uks.sqsh ./build_tmp/mounted_sqsh
 cp ./build_tmp/mounted_sqsh/* ./build_tmp/patched_uks/
-umount ./build_tmp/mounted_sqsh
+sudo umount ./build_tmp/mounted_sqsh
 cat > "./build_tmp/patched_uks/pubdevkey01.pem" << EOF
 -----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDJn1jWU+xxVv/eRKfCPR9e47lP
