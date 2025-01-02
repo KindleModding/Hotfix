959298354f287582ccb6c2e2852cfa8874c64aa2
HackerDude <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-01-02 23:35:33 +0000

    Imagine having negative IQ...

--- src/install-bridge.sh
+++ src/install-bridge.sh
@@ -62,9 +62,6 @@ if [ "$(df -k /var/local | tail -n 1 | awk '{ print $4; }')" -lt "512" ] ; then
     rm -f json_simple-1.1.jar
     rm -f gandalf
     rm -f bridge.conf
-    rm -f appreg_register_sh_integration.sql
-    rm -rf armel
-    rm -rf armhf
     return 1
 fi
 
@@ -180,9 +177,6 @@ rm -f json_simple-1.1.jar
 rm -f gandalf
 rm -f bridge.conf
 rm -f fbink
-rm -f appreg_register_sh_integration.sql
-rm -rf armel
-rm -rf armhf
 logmsg "I" "install" "" "done"
 
 otautils_update_progressbar
