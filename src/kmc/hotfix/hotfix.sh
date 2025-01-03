10cf508c3de54d939297c33799b55544164c27d6
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-01-03 19:48:05 +0000

    Add fancy restarting GUI messsage

--- src/kmc/hotfix/hotfix.sh
+++ src/kmc/hotfix/hotfix.sh
@@ -22,4 +22,8 @@ mntroot ro
 
 logmsg "I" "hotfix" "" "Hotfix Complete! Restarting GUI."
 sleep 3 # So they can read what's about to happen
-restart lab126_gui
+restart lab126_gui &
+sleep 3
+/var/local/kmc/bin/fbink -y 4 -m -S 7 "Restarting GUI"
+/var/local/kmc/bin/fbink -y 6 -m -S 7 "Please Wait..."
+/var/local/kmc/bin/fbink -y 17 -p -S 3 "(Kindles are slow lol)  "
