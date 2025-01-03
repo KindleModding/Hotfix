d8ea546b1399324564eb91ea348e7e10e9e64034
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-01-03 17:53:41 +0000

    Improve hotfix user feedback

--- src/kmc/hotfix/hotfix.sh
+++ src/kmc/hotfix/hotfix.sh
@@ -7,12 +7,17 @@ source /var/local/kmc/hotfix/libhotfixutils
 LOG_DOMAIN="jb_hotfix"
 
 # Here we go...
-logmsg "I" "main" "" "Running HAKT Hotfix (${HOTFIX_VERSION})"
+logmsg "I" "hotfix" "" "Running Universal Hotfix (${HOTFIX_VERSION})"
 
-logmsg "I" "tools" "" "Mounting root as RW"
+logmsg "I" "hotfix" "" "Mounting root as RW"
 mntroot rw
 
 for JOB in ${KMC_PERSISTENT_STORAGE}/hotfix/jobs/*.sh ; do
     logmsg "I" "jobrunner" "" "Running job: $(basename $JOB)"
     source "$(realpath $JOB)"
 done
+
+logmsg "I" "hotfix" "" "Mounting root as RO"
+mntroot ro
+
+logmsg "I" "hotfix" "" "Hotfix Complete! You may need to reboot."
