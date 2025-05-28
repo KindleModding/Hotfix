086b9676156dd0cebc702de44642584bbaff6167
Marek Veselý <notmarek1337@gmail.com>
2025-05-28 22:11:55 +0200

    add ;condmode

--- src/kmc/hotfix/jobs/install_debug_commands_kpp_sys.sh
+++ src/kmc/hotfix/jobs/install_debug_commands_kpp_sys.sh
@@ -2,6 +2,6 @@ if [ -f "/usr/share/app/kpp_sys_cmds.json" ] ; then
 	if ! grep -q "logThis.sh" "/usr/share/app/kpp_sys_cmds.json" ; then
 		logmsg "I" "install_log_kpp" "" "Patching in the dispatch command for kpp"
         # KPP (React, new UI) (sys)
-        sed -e '/^{/a\' -e '    ";log" : "/usr/bin/logThis.sh",' -i "/usr/share/app/kpp_sys_cmds.json"
+        sed -e '/^{/a\' -e '    ";log" : "/usr/bin/logThis.sh", ";condmode" : "/usr/sbin/reboot",' -i "/usr/share/app/kpp_sys_cmds.json"
 	fi
 fi
--- src/kmc/hotfix/jobs/install_debug_commands_pillow.sh
+++ src/kmc/hotfix/jobs/install_debug_commands_pillow.sh
@@ -2,6 +2,6 @@ if [ -f "/usr/share/webkit-1.0/pillow/debug_cmds.json" ] ; then
 	if ! grep -q "logThis.sh" "/usr/share/webkit-1.0/pillow/debug_cmds.json" ; then
 		logmsg "I" "install_log_pillow" "" "Patching in the dispatch command for pillow"
         # Pillow (old UI)
-        sed -e '/^{/a\' -e '    ";log" : "/usr/bin/logThis.sh",' -i "/usr/share/webkit-1.0/pillow/debug_cmds.json"
+        sed -e '/^{/a\' -e '    ";log" : "/usr/bin/logThis.sh", ";condmode" : "/usr/sbin/reboot",' -i "/usr/share/webkit-1.0/pillow/debug_cmds.json"
 	fi
 fi
