c0743a3f7042f59b41081c51a873ad041f1cf6f7
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-10-22 09:14:15 +0100

    Bump version + sh_integration

--- src/kmc/KMCLog.sh
+++ src/kmc/KMCLog.sh
@@ -12,8 +12,7 @@ LOG_PATH="/mnt/us/documents/kmc_log/kmc_log.txt"
 echo "====================" > "$LOG_PATH"
 echo "=   START KMC LOG  =" >> "$LOG_PATH"
 echo "====================" >> "$LOG_PATH"
-cat /etc/version.txt >> "$LOG_PATH"
-cat /etc/prettyversion.txt >> "$LOG_PATH"
+echo "Running Hotfix v2.2.5" >> "$LOG_PATH"
 
 # Dump the file list
 echo >> "$LOG_PATH"
@@ -38,6 +37,9 @@ echo "====================" >> "$LOG_PATH"
 echo "=    THANK YOU.    =" >> "$LOG_PATH"
 echo "=   END KMC LOG  =" >> "$LOG_PATH"
 echo "====================" >> "$LOG_PATH"
+
+cp /etc/version.txt /mnt/us/documents/kmc_log/
+cp /etc/prettyversion.txt /mnt/us/documents/kmc_log/
 
 cp /var/log/messages /mnt/us/documents/kmc_log/var_log_messages
 dmesg >> /mnt/us/documents/kmc_log/dmesg
